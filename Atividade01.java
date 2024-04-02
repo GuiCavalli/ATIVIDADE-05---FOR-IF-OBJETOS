965d0659eba9a7dc2656695f848285dab0797a1b
GuiCavalli <160363168+GuiCavalli@users.noreply.github.com>
2024-04-02 20:28:54 -0300

    Atividade01.java

--- Atividade01.java
+++ Atividade01.java
@@ -4,13 +4,13 @@ public class FizzBuzz {
     public static void main(String[] args) {
         for (int i = 1; i <= 50; i++){
             if (i % 3 == 0 && i % 5 ==0) {
-                System.out.println("FizzBuzz");
+                System.out.println(i + " FizzBuzz");
             }else if(i % 3 ==0) {
-                System.out.println("Fizz");
+                System.out.println(i + " Fizz");
             }else if (i % 5 == 0) {
-                System.out.println("Buzz");
+                System.out.println(i + " Buzz");
             }else{
-                System.out.println(i);
+                System.out.println(i + " não é Divisivel");
             }
         }
     }
